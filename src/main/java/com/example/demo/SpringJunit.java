24abea2df4c8b7efd7c12400a4163a29b8a6d7c2
Kalin Li <ChocolateQiaoli@outlook.com>
2019-12-19 03:45:36 -0600

    Spring Boot Service Unit (#8)
    * Spring Boot Service Unit
    
    Resolve #7
    
    Signed-off-by: Kalin Li <ChocolateQiaoli@outlook.com>
    
    * Spring Boot Service Unit
    
    Resolve #7
    
    Signed-off-by: Kalin Li <ChocolateQiaoli@outlook.com>
    
    * Spring Boot Service Unit
    
    Resolve #7
    
    Signed-off-by: Kalin Li <ChocolateQiaoli@outlook.com>

---
+++ src/main/java/com/example/demo/SpringJunit.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringJunit {
+
+    public int add(int a, int b) {
+         return a + b;
+    }
+}
---
+++ src/test/java/com/example/demo/SpringJunitTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJunitTest {
+    
+    @Autowired
+    private SpringJunit springJunit;
+
+    @Test
+    public void dotest() {
+
+        int res = springJunit.add(11, 22);
+        Assert.assertEquals(res,33);
+    }
+}
